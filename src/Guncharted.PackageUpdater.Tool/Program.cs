11921c6a394cd4885f0ab14e14604f8e450a58a2
ogonchar <hanchar.aleh@gmail.com>
2024-02-06 01:15:47 +0300

    Added project files

---
+++ src/Guncharted.PackageUpdater.Tool/Program.cs
@@ -0,0 +1,19 @@
+﻿using Guncharted.PackageUpdater;
+using McMaster.Extensions.CommandLineUtils;
+
+var app = new CommandLineApplication();
+
+app.HelpOption();
+var packageNameArg = app.Option<string>("-p|--package-name <PACKAGE_NAME>", "Package name to update", CommandOptionType.SingleValue);
+var targetVersionArg = app.Option<string>("-v|--target-version <VERSION>", "Target version of package", CommandOptionType.SingleValue);
+
+app.OnExecuteAsync(async cancellationToken =>
+{
+    var packageName = packageNameArg.Value() ?? string.Empty;
+    var targetVersion = targetVersionArg.Value() ?? string.Empty;
+
+    var executor = new UpdateExecutor();
+    await executor.Run(targetVersion, packageName);
+});
+
+return app.Execute(args);
---
+++ src/Guncharted.PackageUpdater.Tool/UpdateExecutor.cs
@@ -0,0 +1,46 @@
+﻿namespace Guncharted.PackageUpdater;
+
+internal class UpdateExecutor
+{
+    public async Task Run(string targetVersion, string packageName)
+    {
+        if (string.IsNullOrWhiteSpace(targetVersion) || string.IsNullOrWhiteSpace(packageName))
+        {
+            LogConsole("Unable to update versions - incorrect arguments passed");
+            return;
+        }
+
+        await Console.Out.WriteLineAsync(Environment.CurrentDirectory);
+
+        var directoryFiles = Directory.GetFiles(Environment.CurrentDirectory, "*.csproj", SearchOption.AllDirectories);
+
+        foreach (var directoryFile in directoryFiles)
+        {
+            var text = (await File.ReadAllLinesAsync(directoryFile)).ToList();
+
+            var shortFileName = directoryFile.Split('\\').LastOrDefault();
+
+            var targetLine = text.Where((x, i) => x.Contains(packageName) && x.Contains("PackageReference")).FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(targetLine))
+                continue;
+
+            LogConsole($"Setting version of {packageName}-{targetVersion} for {shortFileName}");
+
+            var index = text.IndexOf(targetLine);
+            text[index] = GenerateNewPackageReferenceNode(packageName, targetVersion, targetLine);
+
+            await File.WriteAllLinesAsync(directoryFile, text);
+
+            LogConsole($"Updated package references for {shortFileName}");
+        }
+    }
+
+    private string GenerateNewPackageReferenceNode(string packageName, string targetVersion, string currentRecord)
+    {
+        var newRecord = $"<PackageReference Include=\"{packageName}\" Version=\"{targetVersion}\" />";
+        return currentRecord.Replace(currentRecord.Trim(), newRecord);
+    }
+
+    private void LogConsole(string text) => Console.WriteLine(text);
+}
